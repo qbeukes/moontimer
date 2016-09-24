202ed97768b0d139323f635443e5d963615e600a
Quintin <github.com@last.za.net>
2016-09-24 06:39:28 +0200

    Initial Commit

--- deploy-apk.sh
+++ deploy-apk.sh
@@ -4,9 +4,10 @@ SD=$(cd `dirname $0`; pwd)
 
 APK="$SD/app/build/outputs/apk/app-debug.apk"
 REMOTEDEST="/var/www/simpletimer/download/simple-timer.apk"
+REMOTEDEST="/var/www/simpletimer/download/test.apk"
 REMOTETMP="/tmp/.simpletimer-deploy.tmp"
 
-cat "$APK" | ssh mhm "cat > $REMOTETMP && cp $REMOTETMP $REMOTEDEST && rm -f $REMOTETMP" 
+cat "$APK" | ssh mhm "cat > $REMOTETMP && cp $REMOTETMP $REMOTEDEST ; rm -f $REMOTETMP" 
 ret=$?
 
 if [ $ret -eq 0 ]
